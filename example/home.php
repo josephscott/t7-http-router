60e26dfa3477b8d36d0b1d393258bfc75217d5f5
Joseph Scott <joseph@josephscott.org>
2024-11-09 13:35:59 -0700

    Add example setup

---
+++ example/home.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>T7\HTTP\Router</title>
+</head>
+<body>
+
+<h1>T7\HTTP\Router</h1>
+
+<!--
+t7-server-test-string
+-->
+
+</body>
+</html>
---
+++ example/index.php
@@ -0,0 +1,28 @@
+<?php
+declare( strict_types = 1 );
+
+require __DIR__ . '/../vendor/autoload.php';
+
+class Speak {
+	public function get( array $vars ) {
+		header( 'Content-Type: text/plain' );
+		echo "Speaking ...\n";
+		print_r( $vars );
+	}
+}
+
+$_router = new T7\HTTP\Router(
+	routes: [
+		[ '_404',  __DIR__ . '/404.php' ],
+		[ 'GET', '/', __DIR__ . '/home.php' ],
+		[ 'GET', '/hello/[{name}/]', __DIR__ . '/hello.php' ],
+		[ 'GET', '/function/[{thing}/]', function ( $vars ) {
+			echo "<pre>\n";
+			print_r( $vars );
+			echo "</pre>\n";
+		} ],
+		[ 'GET', '/speak/[{thing}/]', [ 'Speak', 'get' ] ],
+	]
+);
+
+$_router->start();
